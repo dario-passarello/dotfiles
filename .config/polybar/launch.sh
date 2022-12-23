23280ae8127290483b903869ffbcc67285a179d9
Dario Passarello <dario.passarello@gmail.com>
2022-12-23 17:25:58 +0100

    fix: multiscreen & paste

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,10 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-echo "Bars launched..."
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
